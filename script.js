b8d6f71c7583f6d308cf0347bd22ab101ad6b3c9
ShrishtiSingh2211 <142424324+ShrishtiSingh2211@users.noreply.github.com>
2024-02-15 12:56:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //your code here
-
+function strip(bandName) {
+  return bandName.replace(/^(a |the |an )/i, '').trim();
+}
+let sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+let bandsList = document.querySelector('#bands');
+let bandItems = sortedBands.map(band => `<li>${band}</li>`);
+bandsList.innerHTML = bandItems.join('');
